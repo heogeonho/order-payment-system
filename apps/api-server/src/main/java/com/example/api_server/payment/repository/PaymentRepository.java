a48e161060791aa9ba57badce4401bb7b4600b4b
heogeonho <geonh1016@gmail.com>
2025-12-05 18:34:45 +0900

    feat: PaymentRepository 추가

---
+++ apps/api-server/src/main/java/com/example/api_server/payment/repository/PaymentRepository.java
@@ -0,0 +1,12 @@
+package com.example.api_server.payment.repository;
+
+import com.example.api_server.payment.domain.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Optional<Payment> findByOrderId(String orderId);
+}
